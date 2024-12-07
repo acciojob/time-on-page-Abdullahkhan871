92c49b89337324a707eeb9f8883c677e6e134e79
Abdullah khan <akhan283785@gmail.com>
2024-12-07 11:43:01 +0530

    try

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [mount, setMount] = useState(0);
+
+  useEffect(() => {
+    let int = setInterval(() => {
+      setMount((prev) => prev + 1);
+    }, 1000);
+    return () => clearInterval(int);
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <p>You've been on this page for {mount} seconds</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
